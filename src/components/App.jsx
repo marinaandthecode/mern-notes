67a3d4e90b35e94f2fa4d5154380e8571034d5aa
Marina Vaseniova <mvasenyova@gmail.com>
2020-05-28 07:25:53 +0300

    Add create, update, delete functions to notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
 
 function App() {
 
+  const [notes, setNotes] = useState([]); 
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+  function updateNote() {
+    setNotes(prevNotes => {
+      return prevNotes
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    })
+  }
+
   return (
     <div className="App" onDoubleClick>
       <Header />
-      <Note />
+      {notes.map((noteItem, index) => {
+        return <Note key={index} id={index} content={noteItem.content} onAdd={addNote} onDelete={deleteNote} onUpdate={updateNote}/>
+      })}
       <Note />
       <Note />
       <Footer />
@@ -17,3 +40,8 @@ function App() {
 }
 
 export default App;
+
+// 1. Add handleChange fn (update note) and pass as props 
+// 2. Add handleDelete fn (delete note) and pass as props 
+// 3. Add addnote fn (create note) and pass as props 
+// 4. Add notes state []
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Note() {
+function Note(props) {
+
+    const [note, setNote] = useState("");
+
+    function handleInput(event) {
+        const content = event.target.value;
+        setNote(content);
+    }
+
+    function handleDeleteClick() {
+        props.onDelete(props.id);
+    }
+
+    function handleAddClick() {
+        props.onAdd(note);
+    }
+
     return (
         <div className="note" draggable="true" onContextMenu>
-            <textarea className="note-content"></textarea>
+            <textarea onInput={handleInput} className="note-content">{note}</textarea>
+            <button className="note-control add" onClick={handleAddClick}>+</button>
+            <button className="note-control delete" onClick={handleDeleteClick}>x</button>
+        
         </div>
     )
 }
